6f74b877e596d806bb44a93f60593446ee1413ed
Xavi Torelló <info@xaviertorello.cat>
2020-08-24 10:24:13 +0200

    Add Miriade based on work done at https://github.com/astropy/astroquery/pull/377

---
+++ astroquery/miriade/__init__.py
@@ -0,0 +1,32 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+Miriade Query Tool
+------------------
+A tool to query Miriade, The Virtual Observatory Solar System Object
+Ephemeris Generator, a web service of the IMCCE
+http://vo.imcce.fr/webservices/miriade/
+:Author: Julien Woillez (jwoillez@gmail.com) et al.
+"""
+
+from astropy import config as _config
+
+class Conf(_config.ConfigNamespace):
+    """
+    Configuration parameters for `astroquery.miriade`.
+    """
+
+    obs_codes_url = _config.ConfigItem(
+        'http://www.minorplanetcenter.net/iau/lists/ObsCodes.html',
+        'Observatory codes URL from the Minor Planet Center.'
+        )
+    rts_url = _config.ConfigItem(
+        'http://vo.imcce.fr/webservices/miriade/rts_query.php',
+        'URL of the Rise Transit Time web service.'
+        )
+
+conf = Conf()
+
+from .core import Miriade, MiriadeClass
+
+__all__ = ['Miriade', 'MiriadeClass']
+
